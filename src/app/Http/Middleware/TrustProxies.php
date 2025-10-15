484f61d46f4e907f0e2472420304bc2d1e3627d8
camaradigital <appcamaradigital@gmail.com>
2025-10-15 20:44:53 -0300

    Create TrustProxies.php

---
+++ src/app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    // Configura para confiar em TODOS os proxies, pois a DigitalOcean App Platform e o Cloudflare
+    // usam proxies que podem mudar. Esta é a configuração recomendada para apps em nuvem.
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB; // Adicionando AWS_ELB para maior compatibilidade.
+}
