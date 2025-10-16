b73ebae16e36169f4c06f5c615ccad66d6060094
camaradigital <appcamaradigital@gmail.com>
2025-10-16 01:17:09 -0300

    Create SwitchTenantDatabaseTask.php

---
+++ src/app/Multitenancy/Tasks/SwitchTenantDatabaseTask.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tasks;
+
+use Illuminate\Support\Facades\DB;
+use Spatie\Multitenancy\Tasks\SwitchTenantDatabaseTask as BaseSwitchTenantDatabaseTask;
+
+class SwitchTenantDatabaseTask extends BaseSwitchTenantDatabaseTask
+{
+    protected function setTenantConnectionDatabaseName(?string $databaseName)
+    {
+        parent::setTenantConnectionDatabaseName($databaseName);
+
+        $tenantConnectionName = is_null($databaseName)
+            ? $this->landlordDatabaseConnectionName()
+            : $this->tenantDatabaseConnectionName();
+
+        DB::setDefaultConnection($tenantConnectionName);
+    }
+}
