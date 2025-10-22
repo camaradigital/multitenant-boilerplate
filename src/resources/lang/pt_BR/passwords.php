ab6a5b722afea4b3a4ae0a1835f3181e8dae4502
camaradigital <appcamaradigital@gmail.com>
2025-10-22 16:45:24 -0300

    Create passwords.php

---
+++ src/resources/lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Redefinição de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as linhas padrão que correspondem aos motivos
+    | que são dados pelo corretor de senhas para uma tentativa de atualização de senha
+    | que falhou, como por exemplo, um token inválido ou uma nova senha inválida.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos um link de redefinição de senha para o seu e-mail!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com esse endereço de e-mail.", 
+
+];
