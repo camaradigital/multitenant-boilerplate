0c0da85b3d8fe157f36ac602d93037a313f3db30
camaradigital <appcamaradigital@gmail.com>
2025-10-22 16:16:10 -0300

    Update FortifyServiceProvider.php

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -96,10 +96,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(TenantResetUserPassword::class);
-        
-        // Rotas de reset de senha
-        Fortify::requestPasswordResetLinkView(fn () => inertia('Auth/ForgotPassword'));
-        Fortify::resetPasswordView(fn (request) => inertia('Auth/ResetPassword', ['request' => request]));
 
         Fortify::loginView(fn () => inertia('Auth/Login'));
 
